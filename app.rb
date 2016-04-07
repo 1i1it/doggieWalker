380c7e6a64436bea15204f4ff595ef36337d1b85
Liliya <Liliya@Liliyas-MacBook-Pro.local>
2016-04-07 13:25:33 +0300

    first commit

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require './setup'
 
 #functions
 def view_tasks(tasks = nil)  
+  #creates htlm file
   erb(:view, {locals: {tasks: $tasks.find.to_a}})
 end
 
@@ -12,13 +13,35 @@ get '/' do
 end
 
 post '/add' do
-  $tasks.insert({name: params[:task_name], description: params[:task_description]})  
+   # $tasks is the database
+  $tasks.insert({owner: params[:owner_name], name: params[:task_name], description: params[:task_description]})  
   view_tasks
+  redirect back
 end
 
 get '/delete' do
-  $tasks.remove({name: params[:task_name]})
+  #criterea_to_remove = {name: params[:task_name]}
+  criterea_to_remove = {_id: BSON::ObjectId(params[:task_id])}
+
+  $tasks.remove(criterea_to_remove)
   view_tasks
 end
 
+get '/delete_all' do
+  $tasks.remove()
+  redirect back
+end
+
+ get '/mark_completed' do
+  $tasks.update({_id: BSON::ObjectId(params[:task_id])}, {"$set" => {completed: true}})
+  redirect back
+end
+
+  get '/mark_incomplete' do
+    $tasks.update({_id: BSON::ObjectId(params[:task_id])}, {"$set" => {completed: false}})
+    redirect back
+  end
+
+# print to console
 puts "Server is now running."
+
